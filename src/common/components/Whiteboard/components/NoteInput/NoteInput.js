de023d2631837a1e48831a5bca93c9d9e5559181
Jacob Lodenius <jacoblodenius@gmail.com>
2017-09-27 15:16:08 +0200

    added structure to create notes (#3)

--- src/common/components/Whiteboard/components/NoteInput/NoteInput.js
+++ src/common/components/Whiteboard/components/NoteInput/NoteInput.js
@@ -1,9 +1,47 @@
 import React from 'react';
 
-const NoteInput = () => (
-  <h1>
-    NoteInput
-  </h1>
-);
+import noteInputProps from './NoteInput.props';
+
+const NoteInput = (props) => {
+  let title;
+  let color;
+  let itemOne;
+  let itemTwo;
+
+  const handleCreateNoteClick = () => {
+    props.onCreateNote(title.value, 'red', [itemOne.value, itemTwo.value]);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        ref={(c) => { title = c; }}
+      />
+      <button type="button" onClick={handleCreateNoteClick}>
+                Create Note
+      </button>
+
+      {/* not a permanent solution */}
+      <ul>
+        <li>
+          <input
+            type="text"
+            ref={(c) => { itemOne = c; }}
+          />
+        </li>
+        <li>
+          <input
+            type="text"
+            ref={(c) => { itemTwo = c; }}
+          />
+        </li>
+      </ul>
+
+    </div>
+  );
+};
+
+NoteInput.propTypes = noteInputProps;
 
 export default NoteInput;
