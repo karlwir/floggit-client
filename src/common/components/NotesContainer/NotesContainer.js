6aead8460988349a522ad5c67af86f4160e93eb3
jaccohub <jacoblodenius@gmail.com>
2017-10-04 14:06:23 +0200

    reworked state handling to use redux

--- src/common/components/NotesContainer/NotesContainer.js
+++ src/common/components/NotesContainer/NotesContainer.js
@@ -1,24 +1,13 @@
 import { connect } from 'react-redux';
 
-import TodosWrapper from './TodosWrapper';
-import { addTodo, removeTodo } from '../../../reduxStore/config/todos';
-import { updateUserName } from '../../../reduxStore/config/user';
+import NotesWrapper from './NotesWrapper';
+import { addNote, removeNote, updateNote, loadNotes } from '../../../reduxStore/config/notes';
 
 const mapStateToProps = state => ({
-  todos: state.todos,
-  username: state.user.name,
+  notes: state.data,
 });
 
 const mapDispatchToProps = dispatch => ({
-  handleRemove: (id) => {
-    dispatch(removeTodo(id));
-  },
-  handleAdd: (value) => {
-    dispatch(addTodo(value));
-  },
-  handleSetUserName: (name) => {
-    dispatch(updateUserName(name));
-  },
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(TodosWrapper);
+export default connect(mapStateToProps, mapDispatchToProps)(NotesWrapper);
