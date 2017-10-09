2cad8637943f5adbae120d27a62ea90a3ebc66ef
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-10-09 13:25:13 +0200

    Cleaned up some css

--- src/common/components/NotesContainer/components/NoteList/components/Note/Note.js
+++ src/common/components/NotesContainer/components/NoteList/components/Note/Note.js
@@ -13,18 +13,22 @@ const Note = (props) => {
 
   return (
     <div className={`Note ${props.color}`}>
-      <h2>{props.title}</h2>
-      <ul className="generic-list">
-        {props.information.map(item => (
-          <li key={item.id}> {item.text} </li>
-        ))}
-      </ul>
-      <button className="icon-button danger" type="button" onClick={remove}>
-        <i className="fa fa-trash fa-lg" />
-      </button>
-      <button className="icon-button" type="button" onClick={edit}>
-        <i className="fa fa-pencil fa-lg" />
-      </button>
+      <div className="note-content">
+        <h2>{props.title}</h2>
+        <ul className="generic-list">
+          {props.information.map(item => (
+            <li key={item.id}> {item.text} </li>
+          ))}
+        </ul>
+      </div>
+      <div className="note-toolbar">
+        <button className="icon-button danger" type="button" onClick={remove}>
+          <i className="fa fa-trash fa-lg" />
+        </button>
+        <button className="icon-button" type="button" onClick={edit}>
+          <i className="fa fa-pencil fa-lg" />
+        </button>
+      </div>
     </div>
   );
 };
