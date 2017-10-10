809a264ecf891cc1e4d2ad9fcd6c47cdbc38782e
Jacob Lodenius <jacoblodenius@gmail.com>
2017-10-10 15:52:19 +0200

    Filter function (#27)
    * added css folder
    
    * added filter function
    
    * clean up

--- src/common/components/NotesContainer/NotesWrapper.js
+++ src/common/components/NotesContainer/NotesWrapper.js
@@ -6,24 +6,36 @@ import './note-wrapper.css';
 
 import notesWrapperProps from './NotesWrapper.props';
 
-const NotesWrapper = props => (
-  <div className="NotesContainer-wrapper">
-    <NoteFormContainer />
-    <button
-      type="button"
-      className="create-note-button"
-      onClick={props.handleCreateNoteNote}
-    >
+const NotesWrapper = (props) => {
+  const handleFilter = (event) => {
+    props.handleFilter(event.target.value);
+  };
+
+  return (
+    <div className="NotesContainer-wrapper">
+      <NoteFormContainer />
+      <button
+        type="button"
+        className="create-note-button"
+        onClick={props.handleCreateNoteNote}
+      >
       Create new note
-    </button>
-    <NoteList
-      className="NoteList"
-      notes={props.notes}
-      onRemoveNote={props.handleRemoveNote}
-      onUpdateNote={props.handleUpdateNote}
-    />
-  </div>
-);
+      </button>
+      <input
+        type="text"
+        placeholder="Search"
+        className="filter-text-input"
+        onChange={handleFilter}
+      />
+      <NoteList
+        className="NoteList"
+        notes={props.notes}
+        onRemoveNote={props.handleRemoveNote}
+        onUpdateNote={props.handleUpdateNote}
+      />
+    </div>
+  );
+};
 
 NotesWrapper.propTypes = notesWrapperProps;
 
