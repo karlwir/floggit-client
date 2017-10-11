0ae224a9ae558a6e698f78afda3d7e3b6d327c32
JohnMcgrawr <john_r_lindstrom@outlook.com>
2017-10-11 13:22:45 +0200

    Fixed button for clearing search field (#36)
    * Fixed button for clearing search field
    
    * Hide clear input button when no input value

--- src/common/components/NotesContainer/NotesWrapper.js
+++ src/common/components/NotesContainer/NotesWrapper.js
@@ -7,8 +7,23 @@ import './NotesWrapper.css';
 import notesWrapperProps from './NotesWrapper.props';
 
 const NotesWrapper = (props) => {
+  let textInput;
+  let clearButton;
+
   const handleFilter = (event) => {
+    const query = event.target.value;
+    if (query.length > 0) {
+      clearButton.classList.add('visible');
+    } else {
+      clearButton.classList.remove('visible');
+    }
     props.handleFilter(event.target.value);
+  };
+
+  const clearSearchField = () => {
+    textInput.value = '';
+    clearButton.classList.remove('visible');
+    props.handleFilter('');
   };
 
   const renderSearchMessage = () => {
@@ -34,15 +49,26 @@ const NotesWrapper = (props) => {
         >
         Create new note
         </button>
-        <div>
+
+        <div className="InputContainer">
           <input
             type="text"
             placeholder="Search"
             className="filter-text-input"
             onChange={handleFilter}
+            ref={(input) => { textInput = input; }}
           />
-          {renderSearchMessage()}
+          <span
+            className="input-clear-button"
+            onClick={clearSearchField}
+            role="button"
+            tabIndex="0"
+            ref={(span) => { clearButton = span; }}
+          >
+            <i className="fa fa-times-circle" />
+          </span>
         </div>
+        {renderSearchMessage()}
       </div>
       <NoteList
         className="NoteList"
@@ -53,7 +79,6 @@ const NotesWrapper = (props) => {
     </div>
   );
 };
-
 NotesWrapper.propTypes = notesWrapperProps;
 
 export default NotesWrapper;
