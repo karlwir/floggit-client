d89bcb73628ab7615c16cd2fc0b6422bc268d7f8
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-10-12 16:29:03 +0200

    PageHeader component

---
+++ src/common/components/PageHeader/PageHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './PageHeader.css';
+
+const PageHeader = () => (
+  <header className="PageHeader">
+    <div className="logo" /><h1>Flogg<strong>IT</strong></h1>
+  </header>
+);
+
+export default PageHeader;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import NotesContainer from '../../common/components/NotesContainer';
+import PageHeader from '../../common/components/PageHeader';
 import '../../css/master.css';
 import '../../css/font-awesome.min.css';
 
 const Home = () => (
   <div className="HomeContainer">
-    <header>
-      <div className="logo" /><h1>Flogg<strong>IT</strong></h1>
-    </header>
+    <PageHeader />
     <NotesContainer />
   </div>
 );
