d808062a7b978c2e2bc2204045f521b3cb873aa5
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-10-12 16:27:41 +0200

    Added board page

---
+++ src/pages/Board/Board.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NotesContainer from '../../common/components/NotesContainer';
+import PageHeader from '../../common/components/PageHeader';
+import '../../css/master.css';
+import '../../css/font-awesome.min.css';
+
+const Home = () => (
+  <div className="HomeContainer">
+    <PageHeader />
+    <NotesContainer />
+  </div>
+);
+
+export default Home;
