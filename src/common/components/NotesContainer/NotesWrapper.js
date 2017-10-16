1939da8521c3cbd9af176a042da743851672bdd3
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-10-16 17:42:08 +0200

    Searchfilter in shared component

--- src/common/components/NotesContainer/NotesWrapper.js
+++ src/common/components/NotesContainer/NotesWrapper.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import NoteList from './components/NoteList';
+import SearchFilter from '../SearchFilter';
 import LoadSpinner from '../LoadSpinner';
 import './NotesWrapper.css';
 
@@ -10,41 +11,10 @@ class NotesWrapper extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
-
-    this.handleFilter = this.handleFilter.bind(this);
-    this.clearSearchField = this.clearSearchField.bind(this);
-    this.renderSearchMessage = this.renderSearchMessage.bind(this);
   }
 
   componentWillMount() {
     this.props.handleFocusBoard(this.props.boardId);
-  }
-
-  handleFilter(event) {
-    const query = event.target.value;
-    if (query.length > 0) {
-      this.clearButton.classList.add('visible');
-    } else {
-      this.clearButton.classList.remove('visible');
-    }
-    this.props.handleFilter(query);
-  }
-
-  clearSearchField() {
-    this.textInput.value = '';
-    this.clearButton.classList.remove('visible');
-    this.props.handleFilter('');
-  }
-
-  renderSearchMessage(notes) {
-    this.notDisplayed = notes.filter(note => !note.display);
-    this.displayed = notes.filter(note => note.display);
-    return this.notDisplayed.length > 0 ? (
-      <div className="search-message">
-        Search matched
-        <strong>{this.displayed.length}</strong>
-        notes
-      </div>) : '';
   }
 
   render() {
@@ -64,31 +34,11 @@ class NotesWrapper extends React.Component {
               >
                 Create new note
               </button>
-
-              <div className="InputContainer">
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="filter-text-input"
-                  onChange={this.handleFilter}
-                  ref={(input) => { this.textInput = input; }}
-                />
-                <span
-                  className="input-clear-button"
-                  onClick={this.clearSearchField}
-                  role="button"
-                  tabIndex="0"
-                  ref={(span) => { this.clearButton = span; }}
-                >
-                  <i className="fa fa-times-circle" />
-                </span>
-              </div>
-              {this.renderSearchMessage(this.props.notes)}
+              <SearchFilter handleFilter={this.props.handleFilter} items={this.props.notes} />
               <LoadSpinner showWhen={this.props.notesLoading} />
             </div>
             <div>
               <NoteList
-                className="NoteList"
                 notes={this.props.notes}
                 boardId={this.props.boardId}
                 noteFormId={this.props.noteForm.id}
